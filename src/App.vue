f2d952549b7f9bf4dd344953e9125d23cbd10140
Ashmin Karki <ashmin.karki1996@gmail.com>
2019-10-08 17:44:41 +0545

    Day3

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{title}}</h1>
+    <ninjas></ninjas>
+    <app-header></app-header>
+    <app-ninja></app-ninja>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Ninjas from "./Maestro.vue";
+import Header from "./components/header.vue";
+import Footer from "./components/footer.vue";
+import Ninja from "./components/ninjas.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  data: function() {
+    return {
+      title: "Ninja App"
+    };
+  },
   components: {
-    HelloWorld
+    ninjas: Ninjas,
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-ninja": Ninja
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  color: purple;
 }
 </style>
---
+++ src/Maestro.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>List of ninjas</h1>
+    <ul>
+      <li v-for="(ninja,index) in ninjas" v-bind:key="ninja">{{ ninja }}- {{index}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  data: function() {
+    return {
+      ninjas: ["Yoshi", "Ganpachiro", "Ryu"]
+    };
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-top: 60px;
+}
+h1 {
+  color: green;
+}
+</style>
